f68c8665ceecc6e576b9be64935bb895ad39cc3e
Ka-Houl <646676256@qq.com>
2021-07-01 23:23:50 +0800

    update

--- packages/pkg1/lib/pkg1.js
+++ packages/pkg1/lib/pkg1.js
@@ -4,5 +4,5 @@ module.exports = pkg1;
 
 function pkg1() {
     // TODO
-    console.log('pkg1.1')
+    console.log('pkg1.1.')
 }
--- packages/pkg2/lib/pkg2.js
+++ packages/pkg2/lib/pkg2.js
@@ -4,5 +4,5 @@ module.exports = pkg2;
 
 function pkg2() {
     // TODO
-    console.log('pkg2.1')
+    console.log('pkg2.11')
 }
